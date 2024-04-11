d43c8fd0c5016c4eb9d21c4dc1dc4a1dc31ad31a
rifla93 <ralfsemi@yahoo.com>
2024-04-11 12:13:56 +0200

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,47 @@
+#!/usr/bin/python3
+"""
+add new python route
+"""
+
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def index():
+    """
+    return Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """
+    return HBNB!
+    """
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
+def c(text):
+    """
+    return C + text
+    """
     text = text.replace('_', ' ')
-    return "C " + text
+    return 'C ' + text
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+def python(text='is cool'):
+    """
+    return python is cool
+    """
     text = text.replace('_', ' ')
-    return "Python " + text
+    return 'Python ' + text
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
