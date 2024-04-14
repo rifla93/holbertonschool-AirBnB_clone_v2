d00077e3b6e41ce5a258b86c25b832daa4aa34ac
rifla93 <ralfsemi@yahoo.com>
2024-04-14 16:54:53 +0200

    state.py

--- models/state.py
+++ models/state.py
@@ -4,12 +4,12 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship, backref
-from models.city import City
 from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """This is the class for State"""
+    """This is the class for State."""
     __tablename__ = "states"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
@@ -17,11 +17,17 @@ class State(BaseModel, Base):
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """ Returns the list of City"""
-            the_cities = []
-            for c in models.storage.all(City).values():
-                if c.state_id == self.id:
-                    the_cities.append(c)
-            return the_cities
+            """getter attribute cities that returns the
+            list of City instances"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
